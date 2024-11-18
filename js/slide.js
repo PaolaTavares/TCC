97437efd2ca11ea7d0acf7828b49edd884a02236
PAOLA DA SILVA TAVARES <00001105560466sp@aluno.educacao.sp.gov.br>
2024-11-18 16:57:31 -0300

    Atualizando o carrossel e detalhes da navbar

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,8 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+let autoSlideInterval = setInterval(() => plusSlides(1), 4000);
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -13,14 +15,18 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+// Opcional: Pausar o carrossel automático ao interagir com os controles
+document.querySelector('.prev').addEventListener('click', () => clearInterval(autoSlideInterval));
+document.querySelector('.next').addEventListener('click', () => clearInterval(autoSlideInterval));
